1951a86e07667fc2ca5a372a9205aed6f25417bd
ckesana <ckesan2@illinois.edu>
2020-03-23 22:44:12 -0500

    Created a button for inserting values and made a second Class called Story

--- app/src/main/java/com/example/indianapp/MainActivity.java
+++ app/src/main/java/com/example/indianapp/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.indianapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button insert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        insert = findViewById(R.id.Insert);
+        insert.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                openInsert();
+            }
+        });
+    }
+
+    public void openInsert() {
+        Intent intent = new Intent(this, Story.class);
+        startActivity(intent);
     }
 }
